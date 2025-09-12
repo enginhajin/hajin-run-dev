007bcf2a9f60bf997040331a743fc5952d09799d
enginhajin <enginhajin@gmail.com>
2025-09-12 17:30:21 +0900

    feat: improve layout system and styling

--- app/(blog-layout)/layout.tsx
+++ app/(blog-layout)/layout.tsx
@@ -8,18 +8,16 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const showSidebar = pathname.startsWith('/blog');
+  // const showSidebar = pathname.startsWith('/blog');
 
   return (
     <>
       <Header />
       <div className="relative mx-auto mt-16 w-full max-w-196">
+        {/* <SidebarProvider defaultOpen={true} className="min-h-auto"> */}
         <main className="px-6 py-20">{children}</main>
-        {showSidebar && (
-          <SidebarProvider defaultOpen={true}>
-            <Aside />
-          </SidebarProvider>
-        )}
+        {/* {showSidebar && <Aside />} */}
+        {/* </SidebarProvider> */}
       </div>
       <Footer />
     </>
